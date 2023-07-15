b7acceb008e792c7059645551dd659d33e340b31
alexativui <ninobrownalex12@gmail.com>
2023-07-14 21:57:52 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,17 @@
 /**
  * main - Prints the alphabet.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0(Success)
  */
-
 int main(void)
-
 {
-	char c;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for(c = 'a'; c <= 'z';c++ 26;)
-	putchar(c);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
