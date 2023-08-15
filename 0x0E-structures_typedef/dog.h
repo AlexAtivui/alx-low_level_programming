7defacf3342061dd9bac9a2c839c28038db9a23f
alexativui <ninobrownalex12@gmail.com>
2023-08-15 09:56:21 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
+ * struct dog - dog's basic info
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+void printDogInfo(struct dog myDog);
 
 #endif /* DOG_H */
+
