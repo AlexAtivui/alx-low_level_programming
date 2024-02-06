1ff14a63148d0aa6e24e9da66bfcee69a773cef6
AlexAtivui <ninobrownalex12@gmail.com>
2024-02-06 05:08:17 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -18,7 +18,7 @@ void free_listint2(listint_t **head)
 	current = *head;
 	while (current != NULL)
 	{
-		temp = current->next;
+		temporal = current->next;
 		free(current);
 		current = temporal;
 	}
