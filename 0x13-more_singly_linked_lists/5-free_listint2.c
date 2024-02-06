32ddd2bd4c12d12d7adf5ba9f3edbe22a91e1cef
AlexAtivui <ninobrownalex12@gmail.com>
2024-02-06 05:02:00 -0800

    all files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free a listint node
+ * @head: pointer to head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temporal;
+
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
+
+	current = *head;
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current);
+		current = temporal;
+	}
+
+	*head = NULL;
+}
